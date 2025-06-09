ca83bf00f447d0e72fd2c33da58faaeff28fddbe
smriti-67-samanta <samantasmriti0@gmail.com>
2025-06-09 13:28:18 +0530

    Pokemon Explorer

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Dashboard from './pages/Dashboard';
+
+
+const App=()=>{
+return(
+  <div>
+  <h1>Pokemon Detaiils
+  </h1>
+  <Dashboard/>
+  </div>
+)
+}
+  
+
+export default App
